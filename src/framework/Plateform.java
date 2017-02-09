ca547ac7b1d759071a5d237b0d36f6f2f6e8db6a
Killian GOMES <E126956W@S046V7pc14.ensinfo.sciences.univ-nantes.prive>
2017-02-09 15:29:43 +0100

    Ajout du loader dynamique

--- src/framework/Plateform.java
+++ src/framework/Plateform.java
@@ -38,5 +38,54 @@ public class Plateform
 		return o;
 		
 	}
+		
+	public static boolean isDuckSubTypeOf(Class<?> pSuper, Class<?> pSub)
+	{
+		if(pSuper.isAssignableFrom(pSub)) return true;
+		for (Method mSuper : pSuper.getMethods())
+		{
+			try {
+				Method mSub = pSub.getMethod(mSuper.getName(),mSuper.getParameterTypes());
+				if (mSub == null || !mSuper.getReturnType().isAssignableFrom(mSub.getReturnType())) return false;
+			} catch (NoSuchMethodException | SecurityException e) {
+				e.printStackTrace();
+			}
+		}
+		return true;
+	}
+
+	public static Object loadExtension(String filename, Class<?> supercl)
+	{
+		java.util.Properties p = new Properties();
+		
+		try 
+		{
+			p.load(new FileReader(filename));
+		} catch (IOException e1) 
+		{
+			e1.printStackTrace();
+		}
+		
+		Class<?> cl = null;
+		try 
+		{
+			cl = Class.forName((String)p.get("class"));
+			if  (!isDuckSubTypeOf(supercl,cl)) return null;
+		} catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		Object res = null;
+		try 
+		{
+			res = cl.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return res;
+	}
 	
 }
